bf36612cf5eb4548ce89501f45a60426523ee69d
Matheus Mota <devmatheusmota@gmail.com>
2022-12-15 20:34:38 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
-import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.service';
 import { MessagingModule } from '@infra/messaging/messaging.module';
 
 @Module({
-  providers: [],
   imports: [HttpModule, DatabaseModule, MessagingModule],
 })
 export class AppModule {}
